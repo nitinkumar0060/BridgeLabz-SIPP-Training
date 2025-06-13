fcec29fbff75f8b671100f2f6c9381786749b7d1
nitinkumar0060 <nitinyadav5651@gmail.com>
2025-06-13 10:09:32 +0530

    [Nitin] Add. Divisibilitycheck problem

---
+++ Control_Flow/Divisibilitycheck.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Divisibilitycheck{
+public static void main (String []args){
+Scanner Sc=new Scanner(System.in);
+System.out.print("enter number to check divisibility by 5:");
+int n=Sc.nextInt();
+if(n % 5==0){
+System.out.println("Number is divisible by 5");
+}
+else {
+System.out.println("Number is not divisible by 5");
+}
+}
+}
+
+
+
+
+
