ba9e8a7f00678b5eabb9dde215c91d6c0ae22573
nitinkumar0060 <nitinyadav5651@gmail.com>
2025-06-17 08:44:34 +0530

    [Nitin] Add . Program to reverse a number using an array

---
+++ Arrays/DigitFrequency.java
@@ -0,0 +1,39 @@
+package Arrays;
+import java.util.Scanner;
+
+public class DigitFrequency {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        int originalNumber = number;
+        int temp = number;
+        int count = 0;
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+
+        int[] digits = new int[count];
+        for (int i = count - 1; i >= 0; i--) {
+            digits[i] = number % 10;
+            number /= 10;
+        }
+
+        
+        int[] frequency = new int[10];
+        for (int i = 0; i < count; i++) {
+            frequency[digits[i]]++;
+        }
+
+        
+        System.out.println("Digit frequency in number " + originalNumber );
+        for (int i = 0; i < 10; i++) {
+            if (frequency[i] > 0) {
+                System.out.println("Digit " + i + " occurs " + frequency[i] );
+            }
+        }
+
+        
+    }
+}
