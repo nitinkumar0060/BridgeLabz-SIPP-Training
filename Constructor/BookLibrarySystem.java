e725d2885d97d6a357852d757e688a85b81cd96e
nitinkumar0060 <nitinyadav5651@gmail.com>
2025-07-02 09:53:56 +0530

    [Nitin] Add BookLibrarySystem with Book and EBook classes demonstrating access modifiers and author encapsulation

---
+++ Constructor/BookLibrarySystem.java
@@ -0,0 +1,59 @@
+class Book {
+    public String ISBN;           // Public: accessible from anywhere
+    protected String title;       // Protected: accessible in subclass and same package
+    private String author;        // Private: only within Book class
+
+    // Constructor
+    public Book(String ISBN, String title, String author) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+    }
+
+    // Setter for author
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    // Getter for author
+    public String getAuthor() {
+        return author;
+    }
+
+    // Method to display book info
+    public void displayBookInfo() {
+        System.out.println("ISBN: " + ISBN);
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+    }
+}
+
+class EBook extends Book {
+    private double fileSize;
+
+    // Constructor
+    public EBook(String ISBN, String title, String author, double fileSize) {
+        super(ISBN, title, author);
+        this.fileSize = fileSize;
+    }
+
+    public void displayEBookInfo() {
+        System.out.println("EBook Info:");
+        System.out.println("ISBN (Public): " + ISBN);   // Accessing public member
+        System.out.println("Title (Protected): " + title); // Accessing protected member
+        System.out.println("File Size: " + fileSize + " MB");
+        System.out.println("Author (via Getter): " + getAuthor()); // Accessing private member via public getter
+    }
+}
+
+public class BookLibrarySystem {
+    public static void main(String[] args) {
+        Book book = new Book("978-0-13-516630-7", "Java Programming", "James Gosling");
+        book.displayBookInfo();
+
+        System.out.println();
+
+        EBook ebook = new EBook("978-1-23-456789-0", "Effective Java", "Joshua Bloch", 5.4);
+        ebook.displayEBookInfo();
+    }
+}
