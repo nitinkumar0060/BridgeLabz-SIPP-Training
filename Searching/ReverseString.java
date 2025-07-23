87d42c2d21a55abaf9dea0a20b6135e47fddcf07
nitinkumar0060 <nitinyadav5651@gmail.com>
2025-07-23 10:45:31 +0530

    [Nitin] Add.Add program to reverse a string using StringBuilder

---
+++ Searching/ReverseString.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        StringBuilder sb = new StringBuilder();
+        sb.append(input);
+        sb.reverse();
+        String reversed = sb.toString();
+        System.out.println(reversed);
+    }
+}
